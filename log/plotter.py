60e0cf1e7a6a4339416b3122d468436a1d02aaf4
Matthew Kalkoj <dcalco@umich.edu>
2025-04-18 00:16:52 -0400

    Neatness updates

--- log/plotter.py
+++ log/plotter.py
@@ -11,10 +11,7 @@ column_names = ["Time", "UserID", "CircleRadius", "MouseMode",
 csv_file = 'results.csv'  # Replace with your actual CSV file path
 data = pd.read_csv(csv_file, header=None, names=column_names)
 
-unique_radii       =       [10,15,20,25,30]
-mouse_times =   np.array([0.0,0.0,0.0,0.0,0.0])
-one_hand_times =np.array([0.0,0.0,0.0,0.0,0.0])
-two_hand_times =np.array([0.0,0.0,0.0,0.0,0.0])
+unique_radii        =       [10,15,20,25,30]
  
 TPU = 15
 BAR_WIDTH = 0.2
@@ -42,13 +39,7 @@ def average_column(col_name:str):
 COLOR_MOUSE = '#FFCB05'     # Yellow
 COLOR_1H = '#3066C6'        # Light Blue
 COLOR_2H = '#162E5A'        # Dark Blue
-
-# Function to add values on top of bars
-def add_values(ax, bars):
-    for bar in bars:
-        height = bar.get_height()
-        ax.text(bar.get_x() + bar.get_width() / 2, height,
-                f'{height:.2f}', ha='center', va='bottom')
+DPI = 600
 
 def plot_column(col_name:str, plot_name):
     # Plot settings
@@ -56,34 +47,29 @@ def plot_column(col_name:str, plot_name):
 
     plt.rcParams['font.family'] = 'Calibri'
 
-
-    indices = indices = np.arange(len(unique_radii))
-    fig = plt.figure(figsize=(8, 8))
-    b1 = plt.bar(indices, ret[0], BAR_WIDTH, label='Mouse', 
-            color = COLOR_MOUSE)
-    b2 = plt.bar(indices+BAR_WIDTH, ret[1], BAR_WIDTH, label='One Hand',
-            color = COLOR_1H)
-    b3 = plt.bar(indices+2*BAR_WIDTH, ret[2], BAR_WIDTH, label='Two Hand',
-            color = COLOR_2H)
+    indices = np.arange(len(unique_radii))
+    plt.figure(figsize=(8, 8))
+    plt.bar(indices, ret[0], BAR_WIDTH, label='Mouse', 
+       color = COLOR_MOUSE)
+    plt.bar(indices+BAR_WIDTH, ret[1], BAR_WIDTH, label='One Hand',
+       color = COLOR_1H)
+    plt.bar(indices+2*BAR_WIDTH, ret[2], BAR_WIDTH, label='Two Hand',
+       color = COLOR_2H)
 
     plt.xlabel('Circle Radius', fontsize=24)
     plt.ylabel(plot_name, fontsize=24)
-    #plt.ylim(0.0, 0.8)  # Set the y-axis scale
-    #plt.title('Plot 1: Mean ' + plot_name + ' per Circle Radius')
+    #plt.title('Plot 1: Mean ' + plot_name + ' per Circle Radius') ##turn on for title
     plt.yticks(fontsize=18)
     plt.xticks(indices + BAR_WIDTH, unique_radii, fontsize=18)
-    #legend = plt.legend()
+    #legend = plt.legend() ##turn on for legend
     plt.tight_layout()
 
-    plt.savefig("Figures/"+ plot_name+'.png', format='png', dpi=600)
+    plt.savefig("Figures/"+ plot_name+'.png', format='png', dpi=DPI)
 
 def saveLegend():
-    # Define some dummy data and labels for demonstration
-
     plt.rcParams['font.family'] = 'Calibri'
-
     categories = ['Mouse', 'One Hand', 'Two Hands']
-    colors = ['#FFCB05', '#3066C6', '#162E5A']  # Yellow, Light Blue, Dark Blue
+    colors = [COLOR_MOUSE, COLOR_1H, COLOR_2H]  # Yellow, Light Blue, Dark Blue
 
     # Create a dummy figure and axis
     fig, ax = plt.subplots()
@@ -100,19 +86,19 @@ def saveLegend():
     fig_legend.legend(handles=ax.get_legend_handles_labels()[0], labels=categories, loc='center', frameon=False)
 
     # Save only the legend as a separate image file
-    fig_legend.savefig('Figures/legend.png', format='png', bbox_inches='tight', dpi=600)
+    fig_legend.savefig('Figures/legend.png', format='png', bbox_inches='tight', dpi=DPI)
 
 
-# Create the directory
+# Create the directory if not present
 try:
     os.mkdir("Figures/")
     print(f"Directory Figures/ created successfully.")
 except FileExistsError:
     pass
 
-plot_column("MeanTapTime", "Mean time between taps")
-plot_column("TargetsHit", "Targets hit")
-plot_column("Misclicks", "Misclicks")
-
-saveLegend()
+## Saves designated column
+plot_column("MeanTapTime", plot_name="Mean time between taps")
+plot_column("TargetsHit", plot_name="Targets hit")
+plot_column("Misclicks", plot_name="Misclicks")
 #plot_column("MedianTapTime", "Median Tap Time")
+saveLegend()
